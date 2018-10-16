1ddadd3a149016feb24822344a1ae4283673001d
Charles Williams (RIT Student) <crw4147@ad.rit.edu>
2018-10-15 23:49:49 -0400

    Basic Asteroid Movement  - Asteroids move along X and Z axis

---
+++ GGP_Prototype/Assets/Asteroid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.Normalize(new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10))) * speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
